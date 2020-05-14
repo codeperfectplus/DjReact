0472da6baf126ea34bc4a3faa0cde85eb17b4d2c
codeperfectplus <deepak008@live.com>
2020-05-14 08:18:35 +0530

    inital commit

---
+++ apis/views.py
@@ -0,0 +1,11 @@
+from rest_framework import generics 
+from todos import models 
+from .serializers import TodoSerializer
+
+class ListTodo(generics.ListCreateAPIView):
+    queryset = models.Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class DetailTodo(generics.RetrieveDestroyAPIView):
+    queryset = models.Todo.objects.all()
+    serializer_class = TodoSerializer
